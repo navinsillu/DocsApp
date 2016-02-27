5fec013f34d03157db0437807a64ce4eb99a03ba
ev_mac16 <ev_mac16@ev-mac16-2.local>
2016-02-27 14:35:47 +0530

    Version 1.0

---
+++ Version 2.0/DocsApp/DocsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocsApp
+//
+//  Created by ev_mac16 on 27/02/16.
+//  Copyright © 2016 ev_mac16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
